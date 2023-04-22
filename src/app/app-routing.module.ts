444ebb749a60049ac40f0bdefd22c4137b649278
Zeineb <zeineb.walha@esprit.tn>
2023-04-22 22:28:24 +0100

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CreditComponent} from "./credit/credit.component";
+import {FrontComponent} from "./front/front.component";
+
+const routes: Routes = [
+{ path: 'credit', component:CreditComponent },
+  { path: 'front', component:FrontComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FrontComponent } from './front/front.component';
+import { CreditComponent } from './credit/credit.component';
+import { ErrorComponent } from './sidebar/error/error.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    FrontComponent,
+    CreditComponent,
+    ErrorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
